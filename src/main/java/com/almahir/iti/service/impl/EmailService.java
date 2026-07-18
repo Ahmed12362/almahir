34c2664341895ebc0ab4004417ce7390cb663e95
Abdallah Elagamy <abdallahelagamy2@gmail.com>
2026-07-18 15:54:35 +0300

    add email service and configuration for sending emails

---
+++ src/main/java/com/almahir/iti/service/impl/EmailService.java
@@ -0,0 +1,28 @@
+package com.almahir.iti.service.impl;
+
+import com.almahir.iti.dto.request.MailBody;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    // send
+    public void sendEmail(MailBody mailBody) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailBody.to());
+        message.setFrom("test@gmail.com");
+        message.setSubject(mailBody.subject());
+        message.setText(mailBody.text());
+
+        javaMailSender.send(message);
+    }
+
+    //
+}
